109f823d7183823de687a301a41e8398944451bc
dannybauman <goggledefogger@users.noreply.github.com>
2024-10-15 22:46:08 -0700

    don't show the currently processing episode in the list

--- podcast-content-optimizer/frontend/src/components/ManuallyProcessedPodcasts.tsx
+++ podcast-content-optimizer/frontend/src/components/ManuallyProcessedPodcasts.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ProcessedEpisode from './ProcessedEpisode';
 import './ManuallyProcessedPodcasts.css';
+import { usePodcastContext } from '../contexts/PodcastContext';
 
 interface ManuallyProcessedPodcastsProps {
   processedPodcasts: Record<string, any[]>;
@@ -8,18 +9,28 @@ interface ManuallyProcessedPodcastsProps {
 }
 
 const ManuallyProcessedPodcasts: React.FC<ManuallyProcessedPodcastsProps> = ({ processedPodcasts, onDeletePodcast }) => {
+  const { currentJobs } = usePodcastContext();
+
+  // Function to check if an episode is currently being processed
+  const isEpisodeInProgress = (podcastTitle: string, episodeTitle: string) => {
+    return currentJobs.some(job =>
+      job.podcast_name === podcastTitle && job.episode_title === episodeTitle
+    );
+  };
+
   return (
     <section className="manually-processed-podcasts" aria-labelledby="manually-processed-heading">
       <h3 id="manually-processed-heading">Manually Processed Episodes</h3>
       <ul className="processed-list">
         {Object.entries(processedPodcasts).map(([rssUrl, episodes]) => (
-          episodes.map((podcast, index) => (
-            <ProcessedEpisode
-              key={`${rssUrl}-${index}`}
-              podcast={podcast}
-              onDelete={onDeletePodcast}
-            />
-          ))
+          episodes.filter(podcast => !isEpisodeInProgress(podcast.podcast_title, podcast.episode_title))
+            .map((podcast, index) => (
+              <ProcessedEpisode
+                key={`${rssUrl}-${index}`}
+                podcast={podcast}
+                onDelete={onDeletePodcast}
+              />
+            ))
         ))}
       </ul>
     </section>
